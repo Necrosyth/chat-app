dabc2ff1c1bb0ee80811b56b7a838cf88b3a0019
Necrosyth <acoustify69420@gmail.com>
2024-05-21 18:29:16 +0530

    initial

---
+++ react-firebase-chat/src/components/chats/Chats.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./chats.css"
+const Chats = () => {
+  return (
+    <div className='chats'>
+      Chats     
+    </div>
+  )
+}
+
+export default Chats
---
+++ test_proj/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TestPage from './components/TestPage'
+const App = () => {
+  return (
+    <>
+      <TestPage/>
+    </>
+  )
+}
+
+export default App
